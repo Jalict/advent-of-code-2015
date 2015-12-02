0428fd59a1f12c896467ac578d8509f747094587
Frans Larsen <larsen.frans@gmail.com>
2015-12-02 12:45:42 +0100

    Added comments for future

--- day-02.py
+++ day-02.py
@@ -5,3 +5,10 @@ buffer = StringIO.StringIO(str.read())
 
 for line in buffer:
     print line
+
+# Create int for square feet of wrapping paper
+# Go through buffer, line
+# substring the line into length, width and height
+# find smallest edge
+# square feet of wrapping paper += (2*l*w + 2*w*h + 2*h*l) + smallest edge
+# Done!
