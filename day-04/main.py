ea3cd78e682b00639949d43c13e75ecfd4c0c3a7
Frans Larsen <larsen.frans@gmail.com>
2015-12-05 19:55:51 +0100

    Day 4 (Second Part, just change indices if first part)

--- day-04/main.py
+++ day-04/main.py
@@ -1,1 +1,13 @@
-str = "yzbqklnj"
+import md5, sys
+key = "yzbqklnj"
+
+num = 9
+print "INT \tHASH"
+for i in range(0, 100000000):
+    m = md5.new(key + str(i)).hexdigest()
+    try:
+        if str(m[:6]) == "000000" and int(m[6:7]) < num:
+            num = int(m[6:7])
+            print str(i) + "\t" + str(m)
+    except ValueError:
+        continue
